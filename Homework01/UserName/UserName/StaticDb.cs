c48f639132db8fc0202683c5ba823808d684f98d
Danilo Borozan <daniloborozan07@gmail.com>
2021-09-11 01:37:59 +0200

    Update

---
+++ Homework01/UserName/UserName/StaticDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserName
+{
+    public static class StaticDb
+    {
+        public static List<string> UserName = new List<string>()
+        {
+            "Danilo",
+            "Anamarija",
+            "Tanja",
+            "Aleksandar",
+            "Rozeta",
+            "Sime",
+            "Viktor",
+            "Elena",
+            "Ace",
+            "Mila"
+        };
+    }
+}
